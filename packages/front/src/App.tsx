b6107d383b7f5c14709d687665c60374440637ce
Daniel Barbosa de Lima <daniel.lima@ikatec.com.br>
2022-03-09 23:10:45 -0300

    rotas front

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './views/Home'
+import About from './views/About'
+
 const App = () => {
   return (
-    <div >
-      <h1>Welcome</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Router>
   )
 }
 
